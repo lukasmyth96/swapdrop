cc88a074dbae30a0042297a4db2fed85f36f4235
lukasmyth96 <luka@nate.tech>
2020-05-02 10:40:38 +0100

    moved base html and css to homepage app instaed of users - makes more sense to be there

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
