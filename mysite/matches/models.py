626aafb11d7995f87b788d6e290a31cd2d660c3a
SBcharles <sebburgess96@gmail.com>
2020-05-03 12:29:49 +0100

    tried to add table for offers..

--- mysite/matches/models.py
+++ mysite/matches/models.py
@@ -11,3 +11,14 @@ class Like(models.Model):
     def __str__(self):
         abbreviated_product_name = self.product.__str__()[:25]
         return f'{self.liked_by}  --> LIKED -->  {abbreviated_product_name}  --> OWNED BY -->  {self.product.owner}'
+
+
+class Offer(models.Model):
+
+    desired_product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offers_desired')
+    offered_product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offers_offered')
+
+    def __str__(self):
+        abbreviated_desired_product_name = self.desired_product.__str__()[:25] #just truncating.
+        abbreviated_offered_product_name = self.offered_product.__str__()[:25]
+        return f'{abbreviated_offered_product_name}--> OFFERED FOR --> {abbreviated_desired_product_name}'
