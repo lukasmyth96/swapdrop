c036d3c45d18362887447ae01251f88ec81848a3
lukasmyth96 <luka@nate.tech>
2020-05-16 15:53:39 +0100

    Added full status on product corner icon

--- mysite/checkout/urls.py
+++ mysite/checkout/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import shipping_address_redirect
 
 urlpatterns = [
-    path('shipping_address/', shipping_address_redirect, name='shipping_address_redirect'),
+    path('shipping_address/', shipping_address_redirect, name='shipping-address-redirect'),
 ]
