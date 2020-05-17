2d73d38a43545f1f037cb39c35fcfab425f4146d
lukasmyth96 <luka@nate.tech>
2020-05-17 16:28:00 +0100

    basic collection selection view and template

--- mysite/checkout/views.py
+++ mysite/checkout/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -8,10 +10,12 @@ from products.models import Product
 from products.model_enums import ProductStatus
 from swaps.models import Swap
 from swaps.models import SwapStatus
+from bookings.models import TimeSlot, Booking
+from bookings.model_enums import BookingType
 
 
 @login_required()
-def checkout(request, product_id):
+def start_checkout(request, product_id):
     """
     Entry-point for all checkouts - redirects to shipping-address-redirect if address not already given otherwise
     redirects to time-slot picker.
@@ -40,8 +44,53 @@ def shipping_address_redirect(request, product_id):
 @login_required()
 def pick_collection_time(request, product_id):
     if request.method == 'POST':
+        try:
+            selected_time_slot_id = int(request.POST['time-slot-select'])
+        except ValueError:
+            messages.warning(request, 'select an available time slot')
+            return redirect('pick-collection-time', product_id=product_id)  # try again
 
-        # Update status of product
+        # Create collection booking within selected time-slot
+        selected_time_slot = TimeSlot.objects.get(id=selected_time_slot_id)
+        product = Product.objects.get(id=product_id)
+        booking = Booking(time_slot=selected_time_slot,
+                          owner=request.user,
+                          product=product,
+                          booking_type=BookingType.COLLECTION)
+        booking.save()
+        time_slot_str = selected_time_slot.date.strftime('%A') + ' at ' + selected_time_slot.time.label
+        messages.success(request, f'Collection booking confirmed for {time_slot_str}')
+
+        # TODO in future this will move to after payment
+        # Complete Checkout - updates status of Product and Swap
+        checkout_successful = _complete_checkout(product_id)  # TODO what if checkout completion fails?
+        if checkout_successful:
+            messages.success(request, 'Checkout completed successfully')
+        else:
+            messages.error(request, 'Error occurred during checkout completion')
+
+        return redirect('profile')
+
+    else:  # GET request
+        time_slots = TimeSlot.objects.all()  # FIXME starting with all for simplicity
+        context = {'product_id': product_id,
+                   'time_slots': time_slots}
+        return render(request, template_name='checkout/pick_collection_time.html', context=context)
+
+
+
+def _complete_checkout(product_id):
+    """
+    Function to be called upon successful checkout. Updates status of product and Swap.
+
+    Returns
+    -------
+    success: bool
+        True if updated succesfully
+    """
+
+    # Update status of product
+    try:
         product = Product.objects.get(id=product_id)
         product.status = ProductStatus.CHECKOUT_COMPLETE
         product.save(update_fields=['status'])
@@ -51,8 +100,9 @@ def pick_collection_time(request, product_id):
         swap = Swap.objects.get((Q(offered_product=product) | Q(desired_product=product))
                                 & Q(status=SwapStatus.PENDING_CHECKOUT))
         swap.refresh_status()  # internally updates status depending on status of the two products
+        return True
+    except:
+        return False
 
-        messages.success(request, 'Checkout complete')
-        return redirect('profile')
-    else:
-        return render(request, template_name='checkout/pick_collection_time.html', context={'product_id': product_id})
+
+
