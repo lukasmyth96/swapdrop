7987b9054e3315e1884a55f0da794f573569ca4e
lukasmyth96 <luka@nate.tech>
2020-05-23 14:01:39 +0100

    removing old landing page html css

--- mysite/homepage/urls.py
+++ mysite/homepage/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage-home'),
-    path('land/', views.landing_page, name='landing-page')
+    path('', views.landing_page, name='landing-page'),
     ]
--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def homepage(request):
-    return render(request, 'homepage/landing_page.html')
 
 
 def landing_page(request):
-    return render(request, 'homepage/new_landing_page.html')
+    return render(request, 'homepage/landing_page.html')
+
+
