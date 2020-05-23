cf6d7d46af243224bf0f407e595d4cbe2d5b1075
lukasmyth96 <luka@nate.tech>
2020-05-23 17:36:46 +0100

    landing page finished - subject to minor corrections

--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from .forms import LandingPageForm
+
 
 def landing_page(request):
-    return render(request, 'homepage/landing_page.html')
+    context = {}
+    if request.method == 'POST':
+        form = LandingPageForm(request.POST)
+        if form.is_valid():
+            context['submitted'] = True
+            form.save()
+    else:
+        form = LandingPageForm()
+    context['form'] = form
+    return render(request, 'homepage/landing_page.html', context=context)
 
 
