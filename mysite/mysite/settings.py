b8bfb2b5fc334f2c8f3d6195539c6ef6632ff45a
lukasmyth96 <luka@nate.tech>
2020-05-24 16:32:22 +0100

    added config for RDS database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,17 +81,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'swapdrop_db',
-        'USER': 'luka',
-        'PASSWORD': 'test123',
-        'HOST': 'localhost',
-        'PORT': '5432'
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'swapdrop_db',
+            'USER': 'luka',
+            'PASSWORD': 'test123',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 
 
 # Password validation
