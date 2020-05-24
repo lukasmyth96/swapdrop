fb10e9ddaca4b482039a72a8c1e563afa917d32c
lukasmyth96 <luka@nate.tech>
2020-05-24 17:09:36 +0100

    added code to create profile for admin user

--- mysite/users/management/commands/createsu.py
+++ mysite/users/management/commands/createsu.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from users.models import Profile
 
 
 class Command(BaseCommand):
@@ -7,3 +8,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            user = User.objects.get(username='admin')
+            profile = Profile(user=user)
+            profile.save()
