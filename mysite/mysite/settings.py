73004434c36527e559ddb699acf0107681146fbb
lukasmyth96 <luka@nate.tech>
2020-05-24 18:37:25 +0100

    printing logs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,6 +106,9 @@ else:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
+# FIXME remove this after
+print(DATABASES)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
