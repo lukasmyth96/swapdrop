3462e0a03b62e422ed0471551a8dec262074363b
lukasmyth96 <luka@nate.tech>
2020-05-29 12:09:31 +0100

    temporary fix for admin

--- mysite/users/management/commands/createsu.py
+++ mysite/users/management/commands/createsu.py
@@ -6,8 +6,8 @@ from users.models import Profile
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
-            user = User.objects.get(username='admin')
-            profile = Profile(user=user)
-            profile.save()
+        # if not User.objects.filter(username="admin").exists():
+        #     User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        user = User.objects.get(username='admin')
+        profile = Profile(user=user)
+        profile.save()
