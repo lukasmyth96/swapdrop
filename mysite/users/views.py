75d3733d1e0ea61ab20a8c3e393ad0cd844341c7
lukasmyth96 <luka@nate.tech>
2020-05-29 10:52:03 +0100

    automatically logging in new users and redirecting to profile

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from django.views.generic import (
     ListView
 )
@@ -15,13 +16,19 @@ def register(request):
         user_form = UserRegisterForm(request.POST)
         postcode_form = UserPostcodeForm(request.POST)
         if user_form.is_valid() and postcode_form.is_valid():
+            # Create new user and profile
             user = user_form.save()
             # Note - there's probably an easier way of doing below
             profile = Profile.objects.get(user=user)
             profile.postcode = postcode_form.cleaned_data.get('postcode')
             profile.save(update_fields=['postcode'])
-            messages.success(request, f'Account Created! You can now Login')
-            return redirect('login')
+
+            # Automatically log user in
+            user = authenticate(username=user_form.cleaned_data['username'],
+                                password=user_form.cleaned_data['password1'])
+            login(request, user)
+            messages.success(request, f'Welcome to the swapping revolution.')
+            return redirect('profile')
     else:
         user_form = UserRegisterForm()
         postcode_form = UserPostcodeForm()
