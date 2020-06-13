a564fa7e47203d416808c364dd2ef89fb3c2dbaa
lukasmyth96 <luka@nate.tech>
2020-06-13 11:48:02 +0100

    frontend changes for multiple uploads done - brittle

--- mysite/products/forms.py
+++ mysite/products/forms.py
@@ -6,11 +6,14 @@ class ProductCreateForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['image', 'name', 'description', 'gender', 'clothing_type', 'size']
+        fields = ['image', 'image2', 'image3', 'image4', 'name', 'description', 'gender', 'clothing_type', 'size']
         widgets = {'description': forms.Textarea(attrs={'rows': 4})}
 
     def save(self, commit=True):
-        self.instance.cropped_dimensions = [int(dim) for dim in self.data.get('cropped-dimensions').split(',')]
+        self.instance.cropped_dimensions_image = [int(dim) for dim in self.data.get('cropped-dimensions').split(',')]
+        self.instance.cropped_dimensions_image2 = [int(dim) for dim in self.data.get('cropped-dimensions').split(',')]
+        self.instance.cropped_dimensions_image3 = [int(dim) for dim in self.data.get('cropped-dimensions').split(',')]
+        self.instance.cropped_dimensions_image3 = [int(dim) for dim in self.data.get('cropped-dimensions').split(',')]
         return super(ProductCreateForm, self).save(commit=True)
 
 
