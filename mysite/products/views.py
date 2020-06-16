38e2c938c86926657f2e83722e8f301b6e4c3c7a
lukasmyth96 <luka@nate.tech>
2020-06-16 09:36:53 +0100

    fixed succes_url

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
@@ -50,7 +51,7 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductCreateForm
     template_name = 'products/create_form.html'
-    success_url = '/profile'
+    success_url = reverse(viewname='profile-your-items')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -61,7 +62,7 @@ class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     form_class = ProductUpdateForm
     template_name = 'products/update_form.html'
-    success_url = '/profile'
+    success_url = reverse(viewname='profile-your-items')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -76,7 +77,7 @@ class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
-    success_url = '/profile'
+    success_url = reverse(viewname='profile-your-items')
 
     def test_func(self):
         product = self.get_object()
